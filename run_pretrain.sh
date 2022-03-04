13f91bd0dca876db63fb5a1d3649d33e8f2a0985
Morales97 <danimoralesbrotons@gmail.com>
2022-03-04 12:38:46 +0100

    Update run_pretrain.sh

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -9,4 +9,5 @@
 
 
 srun hostname
+srun conda activate PAC2
 srun python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=1001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred 
