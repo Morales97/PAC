0be54705a953659a3f963f02c852e9310c0ab7a1
Morales97 <danimoralesbrotons@gmail.com>
2022-03-04 12:17:13 +0100

    Update run_pretrain.sh

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -5,4 +5,5 @@
 #
 #SBATCH --ntasks=1
 
+srun hostname
 srun python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred
