374111a1a04692131b0dc62de9c5d199b4bbaf63
Morales97 <danimoralesbrotons@gmail.com>
2022-03-04 12:13:45 +0100

    Create run_pretrain.sh

---
+++ run_pretrain.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --job-name=pretrain
+#SBATCH --output=res_pretrain.txt
+#
+#SBATCH --ntasks=1
+
+srun python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred
