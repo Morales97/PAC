30a96d62db5a013882c44f0edd8d412d2c7ad352
Morales97 <danimoralesbrotons@gmail.com>
2022-03-08 10:45:42 +0100

    Update run_pretrain.sh

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -11,5 +11,6 @@
 # srun conda activate PAC2
 # python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=expt2 --use_wandb
 # python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=expt3 --use_wandb --ckpt_freq=1
+module load CUDA/10.0
 srun --gres=gpu:1 python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=601 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=test --use_wandb --ckpt_freq=1
 #srun --pty --gres=gpu:1 bash
