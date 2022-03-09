3a985f6971bb84a017de7c499ba4b0549863f573
Morales97 <danimoralesbrotons@gmail.com>
2022-03-09 17:10:05 +0100

    Update run_pretrain.sh

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -9,6 +9,7 @@
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu_free
 #SBATCH --gres=gpu:1
+#SBATCH --time=06:00:00
 
 
 python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True
