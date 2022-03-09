c73d92f1011dc7c0664968d0830fc41cbc0d3710
Morales97 <danimoralesbrotons@gmail.com>
2022-03-09 17:50:01 +0100

    Update simba_run_pretrain.sh

--- simba_run_pretrain.sh
+++ simba_run_pretrain.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
+#SBATCH --time=06:00:00
 
-
-python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=simba_slurm --ckpt_freq=1 --pre_trained=True
+python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=simba_slurm_2 --ckpt_freq=1 --pre_trained=True
 
