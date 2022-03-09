1631060e421b3b56a8a1ed07b3d4de76b263da12
Morales97 <danimoralesbrotons@gmail.com>
2022-03-09 16:14:19 +0100

    .

--- addnl_scripts/pretrain/rot_pred_no_wandb.py
+++ addnl_scripts/pretrain/rot_pred_no_wandb.py
@@ -228,6 +228,5 @@ if __name__ == '__main__':
 
     #wandb.join()
 
-# python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=101 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True &
-# python addnl_scripts/pretrain/rot_pred.py --resume=expts/rot_pred/checkpoint_2000.pth.tar --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=no_pretrain --ckpt_freq=1 --pre_trained=False &
+# python addnl_scripts/pretrain/rot_pred_no_wandb.py --batch_size=16 --steps=101 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True &
 
--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #
 #SBATCH --job-name=pretrain
-#SBATCH --output=res_pretrain.txt
+#SBATCH --chdir /home/danmoral/PAC
 #
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu
+#SBATCH --qos=gpu_free
 #SBATCH --gres=gpu:1
 
 
-# srun conda activate PAC2
-# python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=expt2 --use_wandb
-# python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=expt3 --use_wandb --ckpt_freq=1
-module load cuda/10.0
-srun python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=601 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=test --use_wandb --ckpt_freq=1
+python addnl_scripts/pretrain/rot_pred_no_wandb.py --batch_size=16 --steps=101 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True
 
-# srun --job-name=pretrain --output=res_pretrain.txt --partition=gpu --gres=gpu:1 python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=601 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=test --use_wandb --ckpt_freq=1
