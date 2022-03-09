1c3404ea06dcbfb0a11bc611e6defd6e785b963b
Morales97 <danimoralesbrotons@gmail.com>
2022-03-09 16:28:30 +0100

    Update run_pretrain.sh

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -11,5 +11,5 @@
 #SBATCH --gres=gpu:1
 
 
-python addnl_scripts/pretrain/rot_pred_no_wandb.py --batch_size=16 --steps=101 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True
+python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True
 
