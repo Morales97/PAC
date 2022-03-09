5f33fbb4f669faac6e924e9e3668d39123b950f7
Morales97 <danimoralesbrotons@gmail.com>
2022-03-09 16:22:53 +0100

    .

--- addnl_scripts/pretrain/rot_pred_no_wandb.py
+++ addnl_scripts/pretrain/rot_pred_no_wandb.py
@@ -228,5 +228,5 @@ if __name__ == '__main__':
 
     #wandb.join()
 
-# python addnl_scripts/pretrain/rot_pred_no_wandb.py --batch_size=16 --steps=101 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True &
+# python addnl_scripts/pretrain/rot_pred_no_wandb.py --batch_size=16 --steps=601 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True &
 
