f6531a9baa1fdb7a82d3da84d2e57431ab5c7444
Morales97 <danimoralesbrotons@gmail.com>
2022-03-10 09:10:08 +0100

    Create simba_run_train.sh

---
+++ simba_run_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --job-name=pretrain
+#SBATCH --chdir /home/danmoral/PAC
+#
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu_free
+#SBATCH --gres=gpu:1
+#SBATCH --time=06:00:00
+
+
+python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True
+
