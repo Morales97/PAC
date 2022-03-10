627b700aaec4fb76bffd707bde4909ac7a8eacb0
Morales97 <danimoralesbrotons@gmail.com>
2022-03-10 12:20:42 +0100

    Update downscaling.py

--- data/downscaling.py
+++ data/downscaling.py
@@ -61,10 +61,10 @@ if __name__ == "__main__":
     n = args.chunk_size
 
     CONVERT_LIST = [
-        ('cityscapes/'+ "leftImg8bit_trainvaltest/leftImg8bit/",
+        ('cityscapes/'+ "leftImg8bit/",
          'cityscapes/' + "leftImg8bit_small/", (1024, 512)),
-        ('gta5/' + "images/",
-         'gta5/' + "images_small/", (1024, 512)),
+        #('gta5/' + "images/",
+        # 'gta5/' + "images_small/", (1024, 512)),
     ]
 
     # Convert files
