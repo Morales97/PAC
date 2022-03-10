2d34ae1a19c45f03c6eaf384ff3dada003d498c0
Morales97 <danimoralesbrotons@gmail.com>
2022-03-10 09:14:32 +0100

    .

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -12,5 +12,5 @@
 #SBATCH --time=06:00:00
 
 
-python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=resnet_seg --ckpt_freq=1 --pre_trained=True
+python addnl_scripts/pretrain/rot_pred.py --batch_size=16 --steps=5001 --dataset=multi --source=real --target=sketch --save_dir=expts/rot_pred --expt_name=test --ckpt_freq=1 --pre_trained=True
 
